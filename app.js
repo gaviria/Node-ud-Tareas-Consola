99dcfdebf17a1c961da8906491dbeb39e3f849ed
Francisco <gaviria.reyes@gmail.com>
2022-08-19 23:18:37 -0500

    Creando menu en consola

---
+++ app.js
@@ -0,0 +1,19 @@
+require("colors");
+const { mostrarMenu, pausa } = require("./helpers/mensajes");
+
+const main = async () => {
+    console.log("hola mundo");
+
+    let opt = "";
+
+    do {
+        opt = await mostrarMenu();
+        console.log({ opt });
+
+        if (opt !== "0") await pausa();
+    } while (opt !== "0");
+
+    //pausa();
+};
+
+main();
---
+++ helpers/mensajes.js
@@ -0,0 +1,47 @@
+require("colors");
+
+const mostrarMenu = () => {
+    return new Promise((resolve, reject) => {
+        console.clear();
+        console.log("==============================".green);
+        console.log("     Seleccione una opción".green);
+        console.log("==============================\n".green);
+
+        console.log(`${"1.".green} Crear Tarea`);
+        console.log(`${"2.".green} Listar Tarea`);
+        console.log(`${"3.".green} Listar Tareas Completadas`);
+        console.log(`${"4.".green} Listar Tareas Pendientes`);
+        console.log(`${"5.".green} Completar Tarea(s)`);
+        console.log(`${"6.".green} Borrar tarea`);
+        console.log(`${"0.".green} Salir\n`);
+
+        const readline = require("readline").createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+
+        readline.question("Seleccione una opción: ", (opt) => {
+            readline.close();
+            resolve(opt);
+        });
+    });
+};
+
+const pausa = () => {
+    return new Promise((resolve, reject) => {
+        const readline = require("readline").createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+
+        readline.question(`\nPresione ${"ENTER".cyan} para continuar\n`, () => {
+            readline.close();
+            resolve();
+        });
+    });
+};
+
+module.exports = {
+    mostrarMenu,
+    pausa,
+};
