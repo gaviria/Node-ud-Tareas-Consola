194f273cda20d2aba938e361fadd48e6628c81e7
Francisco <gaviria.reyes@gmail.com>
2022-08-24 23:38:25 -0500

    creating models

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 require("colors");
-const { inquirerMenu, pausa } = require("./helpers/inquirer");
+const { inquirerMenu, pausa, leerInput } = require("./helpers/inquirer");
+const Tareas = require("./models/tareas");
 
 const main = async () => {
     console.log("hola mundo");
 
     let opt = "";
+    const tareas = new Tareas();
 
     do {
         opt = await inquirerMenu();
-        console.log({ opt });
+
+        switch (opt) {
+            case "1":
+                const desc = await leerInput("Descripción: ");
+                tareas.crearTarea(desc);
+                console.log(desc);
+                break;
+
+            case "2":
+                console.log(tareas._listado);
+                break;
+
+            case "3":
+                break;
+
+            case "4":
+                break;
+
+            case "5":
+                break;
+
+            case "6":
+                break;
+
+            case "0":
+                break;
+        }
 
         await pausa();
     } while (opt !== "0");
